04c720cb6e7c895c85bb8c3f9368a318cc2e4b70
JustRascal00 <Rascal.vamp00@gmail.com>
2024-10-04 18:15:08 +0400

    feat: Add URL reconstruction and indexing logic for RAGChat integration with Redis to prevent duplicate indexing.

--- src/app/[...url]/page.tsx
+++ src/app/[...url]/page.tsx
@@ -1,4 +1,34 @@
-const Page = () => {
+import { redis } from "@/lib/redis"
+import { ragChat } from "@/lib/tag-chat"
+
+interface PageProps {
+    params: {
+        url: string | string[] | undefined
+    }
+}
+
+function reconstructUrl({url}: {url:string[]}) {
+    const decodedComponents = url.map((component) => decodeURIComponent(component))
+    return decodedComponents.join("/")
+}
+
+const Page = async ({params}: PageProps) => {
+    const reconstructedUrl = reconstructUrl({url: params.url as string[]})
+    
+    const isAlreadyIndexed = await redis.sismember("indexed-urls", reconstructedUrl)
+
+    const sessionId = "mock-session"
+
+    if(!isAlreadyIndexed) {
+        await ragChat.context.add({
+            type: "html",
+            source: reconstructedUrl,
+            config: {chunkOverlap: 50, chunkSize: 200},
+        })
+
+        await redis.sadd("indexed-urls", reconstructedUrl)
+    }
+
     return <p>hello</p>
 }
 
